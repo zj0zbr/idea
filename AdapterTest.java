c375c43d6621f6a465cc9348755f53ffbf17e4dc
zj0zbr <m13752362332_1@163.com>
2017-10-15 01:05:46 +0800

    Update AdapterTest.java

--- AdapterTest.java
+++ AdapterTest.java
@@ -8,13 +8,12 @@ class Source{
 
 interface Targetable{
     public void method1();
-
     public void method2();
 }
 
 
 class Adapter extends Source implements Targetable{
-
+//重写method2方法
     @Override
     public void method2() {
         System.out.println("this is targetable method");
@@ -23,8 +22,8 @@ class Adapter extends Source implements Targetable{
 public class AdapterTest {
     public static void main(String[] args) {
         Targetable ta=new Adapter();
-        ta.method1();
-        ta.method2();
+        ta.method1();//调用时直接调用 因为子类继承类父类
+        ta.method2();//子类重写了method2方法
     }
 
 }
