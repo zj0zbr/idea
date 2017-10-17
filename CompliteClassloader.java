0f3f47657361da5ccba19eaaa976d4dafb32a1b7
zj0zbr <m13752362332_1@163.com>
2017-10-17 22:38:54 +0800

    classloder
    自定义classloder

---
+++ CompliteClassloader.java
@@ -0,0 +1,15 @@
+package com.company.classloder;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+public class CompliteClassloader {
+
+    private byte[] getClassData(String filename) throws IOException{
+        File file=new File(filename);
+        byte[] b= new byte[1024];
+        return b;
+    }
+}
